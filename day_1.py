b2729f3c90b5b0a05310199927215e1b9e76b6f8
Thais Lasso <thlasso@pop-os.localdomain>
2022-12-03 18:41:59 -0300

    day 1 part 2

--- day_1.py
+++ day_1.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
+def update_inventories(biggest_inventories, elf_calories):
+    biggest_inventories.sort()
+    if elf_calories > biggest_inventories[0]:
+        biggest_inventories[0] = elf_calories
+    return biggest_inventories
+
 def find_biggest_inventory(inputs):
-    biggest_inventory = elf_calories = 0
+    biggest_inventories = [0, 0, 0]
+    elf_calories = 0
     for cal in inputs:
-        if cal != '\n': elf_calories += int(cal)
-        else:
-            if elf_calories > biggest_inventory:
-                biggest_inventory = elf_calories
+        if cal != '\n': 
+            elf_calories += int(cal)
+        else: 
+            biggest_inventories = update_inventories(biggest_inventories, elf_calories)
             elf_calories = 0
-    return biggest_inventory
+    
+    return sum(biggest_inventories)
 
 if __name__ == '__main__':
     inputs = open('day_1_input.txt', 'r').readlines()
