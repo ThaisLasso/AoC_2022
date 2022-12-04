e609236346f464ddf70ac6c4b45d741545d01f84
Thais Lasso <thlasso@pop-os.localdomain>
2022-12-04 01:54:12 -0300

    day 3 part 2

--- day_3.py
+++ day_3.py
@@ -1,20 +1,20 @@
 def sum_priorities(rucksacks):
     shared_items = []
 
-    for rucksack in rucksacks:
-        shared_items.extend(find_shared_items(rucksack))
-    
-    print(shared_items)
+    groups = [rucksacks[x:x+3] for x in range(0, len(rucksacks), 3)]
+    for group in groups:
+        shared_items.extend(find_shared_items(group))
+
     return sum([find_priority(item) for item in shared_items])
 
 def find_priority(item):
+    if item == '\n': return 0
     number_shift = 96 if item.islower() else 38
     return ord(item) - number_shift
 
-def find_shared_items(rucksack):
-    compartment_1, compartment_2 = dispose_items(rucksack)
-
-    shared = set(item for item in compartment_2 if item in set(compartment_1))
+def find_shared_items(group):
+    rucksack_1, rucksack_2, rucksack_3 = group
+    shared = set(item for item in rucksack_1 if item in set(rucksack_2) and item in set(rucksack_3))
     return shared
 
 def dispose_items(rucksack):
