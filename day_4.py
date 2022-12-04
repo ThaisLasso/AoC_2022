e72fbedfdbcfadbddd86936031e68c8f6abdf908
Thais Lasso <thlasso@pop-os.localdomain>
2022-12-04 03:26:26 -0300

    day 4 part 2

--- day_4.py
+++ day_4.py
@@ -1,14 +1,15 @@
-def fully_contained_assignments(pairs):
+def overlapping_assignments(pairs):
     total = 0
     for pair in pairs: 
         range_1, range_2 = ranges(pair)
-        if either_fully_contains(range_1, range_2):
+        if either_overlaps(range_1, range_2):
             total += 1
     return total
 
-def either_fully_contains(range_1, range_2):
-    contains = lambda o, i: True if o[0] <= i[0] and i[1] <= o[1] else False
-    return contains(range_1, range_2) or contains(range_2, range_1)
+def either_overlaps(range_1, range_2):
+    in_interval = lambda n, i: True if n >= i[0] and n <= i[1] else False
+    overlaps = lambda i1, i2: True if in_interval(i1[0], i2) or in_interval(i1[1], i2) else False
+    return overlaps(range_1, range_2) or overlaps(range_2, range_1)
 
 def ranges(pair):
     elf_1, elf_2 = pair[:-1].split(',')
@@ -18,4 +19,4 @@ def ranges(pair):
 
 if __name__ == '__main__':
     inputs = open('day_4_input.txt', 'r').readlines()
-    print(fully_contained_assignments(inputs))
+    print(overlapping_assignments(inputs))
